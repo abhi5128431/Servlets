835cbed6385844423f23b7992f59ffc9570734f7
abhi5128431 <37515194+abhi5128431@users.noreply.github.com>
2020-11-30 13:53:05 -0800

    Add files via upload

---
+++ displatcher_demo/src/com/SuccessSurvlet.java
@@ -0,0 +1,34 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SuccessSurvlet
+ */
+public class SuccessSurvlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SuccessSurvlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Welcome "+ request.getParameter("username")+
+				" Login Successful at " +new Date()+"</h1>");
+	}
+
+}
